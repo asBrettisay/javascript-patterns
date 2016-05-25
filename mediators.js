ce41121c1162cdfa615e5e8dec8f5c01bba3078d
asBrettisay <brett.caudill.is@gmail.com>
2016-05-25 16:14:27 -0600

    Added to mediator example.

--- mediators.js
+++ mediators.js
@@ -54,3 +54,57 @@ var myMediator = new Mediator();
 
 myMediator.subscribe('foo', Subscriber);
 myMediator.publish('foo', 'chicken');
+
+
+
+// Example where mediator can be installed to a new object.
+
+
+var mediator = (function(){
+    var subscribe = function(channel, fn){
+        if (!mediator.channels[channel]) mediator.channels[channel] = [];
+        mediator.channels[channel].push({ context: this, callback: fn });
+        return this;
+    },
+
+    publish = function(channel){
+        if (!mediator.channels[channel]) return false;
+        var args = Array.prototype.slice.call(arguments, 1);
+        for (var i = 0, l = mediator.channels[channel].length; i < l; i++) {
+            var subscription = mediator.channels[channel][i];
+            subscription.callback.apply(subscription.context, args);
+        }
+        return this;
+    };
+
+    return {
+        channels: {},
+        publish: publish,
+        subscribe: subscribe,
+        installTo: function(obj){
+            obj.subscribe = subscribe;
+            obj.publish = publish;
+        }
+    };
+
+
+
+mediator.name = "tim";
+mediator.subscribe('nameChange', function(arg){
+        console.log(this.name);
+        this.name = arg;
+        console.log(this.name);
+});
+
+mediator.publish('nameChange', 'david'); //tim, david
+
+
+var obj = { name: 'sam' };
+mediator.installTo(obj);
+obj.subscribe('nameChange', function(arg){
+        console.log(this.name);
+        this.name = arg;
+        console.log(this.name);
+});
+
+obj.publish('nameChange', 'john'); //sam, john
